364f6e30ad9dd25b03ec73011c166acc7d10386f
renanluizmacedo <renanluizmacedo@gmail.com>
2021-07-05 16:48:05 -0300

    verificacao admin

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,12 @@ Route::get('/', function () {
 Route::get('/produtos',[ProdutoControlador::class,'index']);
 
 Route::get('/negado',function(){
-    return "Acesso Negado";
+    return "Acesso Negado.Você precisa estar logado para acessar esta pagina!";
 })->name('negado');
+
+Route::get('/negadologin',function(){
+    return "Acesso Negado!Você não tem os privilégios de administrador!";
+})->name('negadologin');
 
 Route::post('/login',function(Request $req){
 
@@ -32,9 +36,12 @@ Route::post('/login',function(Request $req){
     switch($req->input('user')){
         case 'renan': 
             $login_ok = $req->input('passwd') === "senharenan";
+            $admin = true;
             break;
         case 'rafael':
             $login_ok = $req->input('passwd') === "senharafael";
+            $admin = false;
+
             break;
         case 'default':
             $login_ok = false;
@@ -42,7 +49,7 @@ Route::post('/login',function(Request $req){
     }
 
     if($login_ok){
-        $login = ['user' => $req->input('user')];
+        $login = ['user' => $req->input('user'),'admin' => $admin];
         $req->session()->put('login',$login);
         return response("Login OK",200);
 
