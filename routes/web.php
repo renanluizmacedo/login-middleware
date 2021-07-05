98ed77c0506f6ec2dda316137e2b349a90f88b6b
renanluizmacedo <renanluizmacedo@gmail.com>
2021-07-05 10:41:17 -0300

    erro rota post

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,35 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/produtos',[ProdutoControlador::class,'index']);
+
+Route::post('/login',function(Request $req){
+
+    $login_ok = false;
+
+    switch($req->input('user')){
+        case 'renan': 
+            $login_ok = $req->input('passwd') === "senharenan";
+            break;
+        case 'rafael':
+            $login_ok = $req->input('passwd') === "senharafael";
+            break;
+        case 'default':
+            $login_ok = false;
+
+    }
+
+    if($login_ok){
+        return response("Login",200);
+
+    }
+    else{
+        return response("Erro no login",404);
+    }
+});
